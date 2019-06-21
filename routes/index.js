bd400a14ac53de34cd0d6930358154991bad5701
arcynosure <arcynosure@gmail.com>
2019-06-21 12:58:04 +0530

    stage 1 api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+const BnbApiClient = require('@binance-chain/javascript-sdk');
+const WAValidator = require('wallet-address-validator');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/step1', function(req, res) {
+  if(validateBNBAddress(req.body.bnbaddress)){
+    if(validateETHAddress(req.body.ethaddress)){
+      if(validateAmount(req.body.amount)){
+
+      }
+      else{
+        return res.send({
+          status:false,
+          message:'Invalid amount'
+        })
+      }
+
+    }
+    else{
+      return res.send({
+        status:false,
+        message:'Invalid Eth Address'
+      })
+    }
+  }
+  else{
+    return res.send({
+      status:false,
+      message:'Invalid bnb address'
+    })
+  }
+  res.render('step1');
 });
+
+function validateBNBAddress(address) {
+  const addressValid = BnbApiClient.crypto.checkAddress(address);
+  return addressValid
+}
+
+function validateETHAddress(valid) {
+  const valid = WAValidator.validate(address, 'ETH');
+  return valid
+}
+
+function validateAmount(amount) {
+  if (typeof amount != "number") {
+    return amount
+  }
+}
 
 module.exports = router;
